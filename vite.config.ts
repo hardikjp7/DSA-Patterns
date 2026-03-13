d2dd190c361ba92ff4620aa550441e0c91fd7675
Hardik Parmar <hardikjparmar7@gmail.com>
2026-03-13 20:52:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
-  base: "/",
+  base: "/DSA-Patterns/",
   plugins: [
     react(),
     tailwindcss(),
